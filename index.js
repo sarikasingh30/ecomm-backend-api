75496a72f4d74fe51631dd6492f2ac792bee0317
Sarika <sarika.goooogle.1330@gmail.com>
2024-06-05 14:33:08 +0530

    added port

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
-
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
-
-const PORT =8080
-
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use("/api", router);
+server.listen(PORT, () => console.log("Running on port 3000"));
